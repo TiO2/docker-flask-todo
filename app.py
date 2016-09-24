3f131c0992d69cfa6bd3d33b18ec759f087bccca
tio2 <pema@syr.edu>
2016-09-24 11:14:35 -0400

    changed the mongodb docker image version and the docker environmental variables.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient(
-    os.environ['TODO_DB_1_PORT_27017_TCP_ADDR'],
+    os.environ['DB_PORT_27017_TCP_ADDR'],
     27017)
 db = client.tododb
 
